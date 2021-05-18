3bb9b333e98f38bfebd1968baa11ecfadd3fb127
scottalexgray <scottalexgray@gmail.com>
2021-05-18 14:11:34 +0200

    Initial Commit

---
+++ examples/usage/usage.ino
@@ -0,0 +1,18 @@
+// Example usage for HX711_SWE library by Scott Alexander Gray.
+
+#include "HX711_SWE.h"
+
+// Initialize objects from the lib
+HX711_SWE hX711_SWE;
+
+void setup() 
+{
+    // Call functions on initialized library objects that require hardware
+    
+}
+
+void loop() 
+{
+    // Use the library's initialized objects and functions
+    
+}
